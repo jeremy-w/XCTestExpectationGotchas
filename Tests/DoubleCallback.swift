b40461239ff2ed24634fba3cabfe231bfb735e8e
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2016-03-19 23:33:15 -0400

    Work around double callback
    Done by annihilating the weak ref to the promise after the first callback.

--- Tests/DoubleCallback.swift
+++ Tests/DoubleCallback.swift
@@ -1,13 +1,32 @@
 import XCTest
 
 class DoubleCallback: XCTestCase {
-    func testDoubleTheFulfillment() {
+    func xtestDoubleTheFulfillment() {
         let promiseToCallBack = expectationWithDescription("calls back")
         let callBackDelay: NSTimeInterval = 1
 
         twiceAfter(seconds: callBackDelay) {
             print("i hear you calling me")
             promiseToCallBack.fulfill()
+        }
+
+        let afterCallBack = 2 * callBackDelay
+        waitForExpectationsWithTimeout(afterCallBack, handler: nil)
+    }
+
+
+    func testSafelyDoubleTheFulfillment() {
+        weak var promiseToCallBack = expectationWithDescription("calls back")
+        let callBackDelay: NSTimeInterval = 1
+
+        twiceAfter(seconds: callBackDelay) {
+            guard let promise = promiseToCallBack else {
+                print("once was enough, thanks!")
+                return
+            }
+
+            promise.fulfill()
+            promiseToCallBack = nil
         }
 
         let afterCallBack = 2 * callBackDelay
