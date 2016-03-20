156612294a0dc12d9facb23cf77ef9b4bc5754dd
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2016-03-19 23:57:29 -0400

    Demonstrate using your own promises

---
+++ Tests/BringYourOwnPromises.swift
@@ -0,0 +1,34 @@
+import XCTest
+import Deferred
+
+
+class BringYourOwnPromises: XCTestCase {
+    func testGettingAPony() {
+        let anyDelay: NSTimeInterval = 1
+        let futurePony = giveMeAPony(after: anyDelay)
+
+        let longEnough = anyDelay + 1
+        guard let pony = futurePony.wait(.Interval(longEnough)) else {
+            XCTFail("no pony ;_;")
+            return
+        }
+
+        print("we got a pony! \(pony)")
+    }
+}
+
+
+
+func giveMeAPony(after seconds: NSTimeInterval) -> Future<Pony> {
+    let eventualPony: Deferred<Pony> = Deferred()
+    after(seconds: seconds) {
+        let pony = Pony()
+        print("dreams come true, have a pony: \(pony)")
+        eventualPony.fill(pony)
+    }
+    return Future(eventualPony)
+}
+
+
+
+class Pony {}
