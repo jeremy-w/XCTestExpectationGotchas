c245481b2c497d150c221e7956824b2f7bf26a4d
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2016-03-19 22:21:24 -0400

    Shift `after(seconds:call:)` to end of file
    What you want to see when you open the file is the test,
    not the helper that's named clearly enough that you can
    probably guess what it does withou needing to read it.

--- Tests/LateCallback.swift
+++ Tests/LateCallback.swift
@@ -1,19 +1,5 @@
 import XCTest
 
-func after(seconds seconds: NSTimeInterval, call closure: () -> Void) {
-    let delay = dispatch_time(
-        DISPATCH_TIME_NOW,
-        Int64(seconds) * numericCast(NSEC_PER_SEC))
-    let ontoQueue = dispatch_get_main_queue()
-    dispatch_after(delay, ontoQueue) {
-        print("\(seconds): finished waiting")
-        closure()
-        print("\(seconds): all done here")
-    }
-}
-
-
-
 class LateCallback: XCTestCase {
     let callBackDelay: NSTimeInterval = 2
 
@@ -28,5 +14,19 @@ class LateCallback: XCTestCase {
         waitForExpectationsWithTimeout(callBackDelay / 2) { error in
             print("Aww, we timed out: \(error)")
         }
+    }
+}
+
+
+
+func after(seconds seconds: NSTimeInterval, call closure: () -> Void) {
+    let delay = dispatch_time(
+        DISPATCH_TIME_NOW,
+        Int64(seconds) * numericCast(NSEC_PER_SEC))
+    let ontoQueue = dispatch_get_main_queue()
+    dispatch_after(delay, ontoQueue) {
+        print("\(seconds): finished waiting")
+        closure()
+        print("\(seconds): all done here")
     }
 }
